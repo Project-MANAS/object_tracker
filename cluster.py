021f27ebd16c22580345aa370a032a577cb6d650
Chaitanya <chaitanya2.1999@gmail.com>
2018-12-30 18:16:27 +0530

    Added visualization options. Added support for tracking frames smaller than the input image frame

--- cluster.py
+++ cluster.py
@@ -54,14 +54,12 @@ class ObjectTracker:
                                                     [0., 1., 0., 0.]
                                                     ], np.float64)
         kalman_filter.processNoiseCov = 1e-5 * np.eye(4, dtype=np.float64)
-        kalman_filter.measurementNoiseCov = 1e-1 * np.eye(2, dtype=np.float64)
+        kalman_filter.measurementNoiseCov = 1e-8 * np.eye(2, dtype=np.float64)
         kalman_filter.errorCovPost = 1. * np.ones((4, 4), np.float64)
         kalman_filter.statePost = 0.1 * np.random.randn(4, 1)
         init_state = np.array(list(center) + [0.0, 0.0], np.float64)  # [x, y] + [dx, dy]
         kalman_filter.statePre = init_state.transpose()
-        estimate = kalman_filter.correct(np.array(center, dtype=np.float64))
-        print("Estimate: " + str(estimate))
-        print("State: " + str(kalman_filter.statePost))
+        kalman_filter.correct(np.array(center, dtype=np.float64))
         obj_id = time.time()
         return TrackedObject(obj_id, init_state, kalman_filter)
 
@@ -69,21 +67,24 @@ class ObjectTracker:
         predicted_objects = []
         for obj in self.objects:
             prediction = obj.tracker.predict()
-            if self.verbose:
-                print("Prediction: " + str(prediction))
             predicted_objects.append(TrackedObject(obj.id, np.transpose(prediction), obj.tracker))
         # print(predicted_objects)
         return predicted_objects
+
+    def is_within_tracking_frame(self, point):
+        if point[0] < self.x_min \
+                or point[0] > self.x_max \
+                or point[1] < self.y_min \
+                or point[1] > self.y_max:
+            return False
+        return True
 
     def update_tracked_objects(self, centers, predicted_objects):
         updated_objects = []
         for obj in predicted_objects:
             # stop tracking objects outside the tracking frame
-            if obj.state[0] < self.x_min  \
-                    or obj.state[0] > self.x_max \
-                    or obj.state[1] < self.y_min \
-                    or obj.state[1] > self.y_max:
-                print("deleting object: " + str(obj.id))
+            if not self.is_within_tracking_frame((obj.state[0], obj.state[1])):
+                print("deleting object: " + str(obj))
                 continue
 
             # Not all objects we were tracking were detected in the frame
@@ -92,12 +93,16 @@ class ObjectTracker:
                 break
 
             closest_center_idx = 0
+            obj_center = np.array([obj.state[0], obj.state[1]])
             for i, center in enumerate(centers):
                 closest_center = np.squeeze(centers[closest_center_idx])
-                obj_center = np.array([obj.state[0], obj.state[1]])
                 if ObjectTracker.dist(obj_center, center) < \
                         ObjectTracker.dist(obj_center, closest_center):
                     closest_center_idx = i
+
+            if self.verbose:
+                print("Obj state: " + str(obj.state))
+                print("Closest center: " + str(centers[closest_center_idx]))
 
             estimate = obj.tracker.correct(centers[closest_center_idx])
 
@@ -110,8 +115,9 @@ class ObjectTracker:
             # centers = centers[0:closest_center_idx] + centers[closest_center_idx+1:]
 
         for center in centers:
-            print("Creating new object for: " + str(center))
-            updated_objects.append(ObjectTracker.create_object(center))
+            if self.is_within_tracking_frame(center):
+                print("Creating new object for: " + str(center))
+                updated_objects.append(ObjectTracker.create_object(center))
         self.objects = updated_objects
 
     def get_objects(self):
@@ -137,9 +143,10 @@ def print_usage():
 def main(argv):
     verbose = False
     infile = ""
+    show_obj_info = False
 
     try:
-        opts, args = getopt.getopt(argv, "hvi:", ["help", "verbose", "input="])
+        opts, args = getopt.getopt(argv, "hvsi:", ["help", "verbose", "show-info", "input="])
     except getopt.GetoptError:
         print_usage()
         return
@@ -147,10 +154,12 @@ def main(argv):
     for opt, arg in opts:
         if opt=='-i' or opt=='--input':
             infile = arg
-        if opt=='-v' or opt=='--verbose':
+        elif opt=='-v' or opt=='--verbose':
             verbose = True
-        if opt=='-h' or opt=='--help':
+        elif opt=='-h' or opt=='--help':
             print_usage()
+        elif opt=='-s' or opt=='--show-info':
+            show_obj_info = True
 
     if infile:
         vid = cv2.VideoCapture(infile)
@@ -159,7 +168,9 @@ def main(argv):
         print("path to video not provided")
         return
 
-    tracker = ObjectTracker(x_max=1024, y_max=768, verbose=verbose)
+    tracker = ObjectTracker(x_max=1013, y_max=757, verbose=verbose)
+    font = cv2.FONT_HERSHEY_PLAIN
+
     while vid.isOpened():
         if verbose:
             print("Reading...")
@@ -169,12 +180,16 @@ def main(argv):
         img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
         thresh, img = cv2.threshold(img, 127, 255, 0)
         tracker.track_frame(img)
-        centers = [tuple(obj[1][0:2]) for obj in tracker.get_objects()]
 
-        for point in centers:
-            if point is None:
-                continue
+        for obj in tracker.get_objects():
+            point = tuple(obj.state[0:2])
             cv2.circle(orig_img, (int(point[0]), int(point[1])), 5, (255, 0, 0))
+            if show_obj_info:
+                label = "id: " + str(obj.id) \
+                        + " vx: " + str(obj.state[2]) \
+                        + " vy: " + str(obj.state[3])
+                cv2.putText(orig_img, label, (int(point[0]+3), int(point[1]+3)),
+                            font, 1, (0, 255, 0), 1, cv2.LINE_AA)
 
         cv2.imshow("output", orig_img)
         cv2.waitKey(10)
