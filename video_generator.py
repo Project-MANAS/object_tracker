d49a42d1a97030dc6b1d3675df7b055088f0bd67
Chaitanya <chaitanya2.1999@gmail.com>
2018-12-30 21:41:21 +0530

    Video generator: Added additional options to add noise, control the number of objects present at a time. Objects can now start moving from any edge and in any any direction that causes it to move through the frame

--- video_generator.py
+++ video_generator.py
@@ -4,18 +4,48 @@ import getopt
 import cv2
 import numpy as np
 import random
+from recordclass import recordclass
+
+State = recordclass('State', ['x', 'y', 'vx', 'vy'])
 
 
 def print_usage():
     print("Usage: ./video_generator.py -o <output file> [OPTIONS]")
 
 
+def generate_init_states(num_objects, vx, vy, resolution):
+    states = []
+    for i in range(num_objects):
+        x = int(resolution[0]*random.random())
+        y = int(resolution[1]*random.random())
+        direction = -1 if random.random() < 0.5 else 1
+        choices = [
+            State(x, 0, direction*vx, vy),                 # top edge
+            State(x, resolution[1]-1, direction*vx, -vy),  # bottom edge
+            State(0, y, vx, direction*vy),                 # left edge
+            State(resolution[0]-1, y, -vx, direction*vy)   # right edge
+        ]
+        states.append(random.choice(choices))
+
+    return states
+
+
+def is_outside_frame_bounds(point, resolution):
+    if point[0] > resolution[0] \
+            or point[0] < 0 \
+            or point[1] > resolution[1] \
+            or point[1] < 0:
+        return True
+    return False
+
+
 def main(argv):
     try:
         opts, args = getopt.getopt(
             argv,
-            "ho:l:x:y:r:",
-            ["help", "outfile=", "loops=", "hres=", "yres=", "radius=", "maxvelx=", "maxvely="]
+            "ho:l:x:y:r:n:b:",
+            ["help", "outfile=", "loops=", "hres=", "yres=", "radius=", "velx=", "vely=",
+             "noise-factor=", "num-objects="]
         )
     except getopt.GetoptError:
         print_usage()
@@ -25,9 +55,11 @@ def main(argv):
     num_loops = 1
     h_res = 1024
     v_res = 768
-    vx_max = 5.0
-    vy_max = 5.0
+    vx = 5.0
+    vy = 5.0
     radius = 15
+    noise_factor = 0
+    num_objects = 1
 
     for opt, arg in opts:
         if opt=='-h' or opt=='--help':
@@ -42,13 +74,14 @@ def main(argv):
             v_res = int(arg)
         elif opt=='-r' or opt=='--radius':
             radius = float(opt)
-        elif opt == '--maxvelx':
-            vx_max = float(opt)
-        elif opt == '--maxvely':
-            vy_max = float(opt)
-        else:
-            print("Unknown option "+opt)
-            print_usage()
+        elif opt=='-n' or opt=='--noise-factor':
+            noise_factor = float(arg)
+        elif opt=='-b' or opt=='--num-objects':
+            num_objects = int(arg)
+        elif opt == '--velx':
+            vx = float(opt)
+        elif opt == '--vely':
+            vy = float(opt)
 
     if outfile:
         fourcc = cv2.VideoWriter_fourcc(*'avc1')
@@ -59,18 +92,19 @@ def main(argv):
         return
 
     for _ in range(num_loops):
-        x = 0
-        y = random.random()*v_res
-        vx = random.random()*vx_max
-        vy = random.random()*vy_max
-        while x<=h_res and y<=v_res:
+        states = generate_init_states(num_objects, vx, vy, (h_res, v_res))
+        while states:
             frame = np.zeros((v_res, h_res, 3), dtype=np.uint8)
-            cv2.circle(frame, (int(x), int(y)), radius, (255, 255, 255), thickness=-1)
+            for i in range(len(states)):
+                cv2.circle(frame, (int(states[i].x), int(states[i].y)),
+                           radius, (255, 255, 255), thickness=-1)
+                states[i].x += states[i].vx + random.uniform(-noise_factor, noise_factor)
+                states[i].y += states[i].vy + random.uniform(-noise_factor, noise_factor)
+                if is_outside_frame_bounds((states[i].x, states[i].y), (h_res, v_res)):
+                    del states[i]
             vid.write(frame)
-            x += vx
-            y += vy
-            #cv2.imshow('frame', frame)
-            #cv2.waitKey(1)
+            cv2.imshow('frame', frame)
+            cv2.waitKey(1)
 
     vid.release()
 
